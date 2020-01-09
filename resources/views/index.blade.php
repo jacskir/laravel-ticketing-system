2fd3238ec5a5f8ff4731779dc27db08e32281ab1
Jacob Frampton-Skirrow <jacskir@gmail.com>
2020-01-09 16:17:11 +0000

    Add base route to show all Tickets

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <title>Ticketing System</title>
+</head>
+<body>
+    <table class="table is-striped is-hoverable">
+        <thead>
+            <th>Assigned to</th>
+            <th>Ticket</th>
+            <th>Status</th>
+            <th>Date</th>
+        </thead>
+        <tbody>
+            @foreach ($tickets as $t)
+                <tr>
+                    <td>{{ $t -> name }}</td>
+                    <td>{{ $t -> ticket }}</td>
+                    <td>{{ $t -> status }}</td>
+                    <td>{{ $t -> created_at->format('D jS F') }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
