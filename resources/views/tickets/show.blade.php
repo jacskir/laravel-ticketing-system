cd94abda3d55a46c4ec6d5f3e26843c57cb23dd2
Jacob Frampton-Skirrow <jacskir@gmail.com>
2020-01-11 16:38:19 +0000

    Add template, modify ticket view page to use it

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -1,16 +1,34 @@
-<table class="table is-striped">
-    <tbody>
-        <tr>
-            <td>Assigned to:</td>
-            <td>{{ $ticket -> name }}</td>
-        </tr>
-        <tr>
-            <td>Date:</td>
-            <td>{{ $ticket -> created_at -> format('D jS F') }}</td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>{{ $ticket -> ticket }}</td>
-        </tr>
-    </tbody>
-</table>
+@extends ('layouts.app')
+
+@section ('page_title')
+    Ticketing System | Ticket assigned to {{ $ticket -> name }}
+@endsection
+
+@section ('page_heading')
+    Ticket assigned to {{ $ticket -> name }}
+@endsection
+
+@section ('content')
+<div class="box">
+    <table class="table is-striped">
+        <tbody>
+            <tr>
+                <td>Assigned to:</td>
+                <td>{{ $ticket -> name }}</td>
+            </tr>
+            <tr>
+                <td>Date:</td>
+                <td>{{ $ticket -> created_at -> format('D jS F') }}</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>{{ $ticket -> ticket }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<p>
+    <a class="button" href="/">Back</a>
+</p>
+@endsection
