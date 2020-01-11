690d40a855cd0f745922fcb10e5741c25acf1d64
Jacob Frampton-Skirrow <jacskir@gmail.com>
2020-01-11 17:02:29 +0000

    Add routes for ticket editing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'TicketController@index');
 Route::get('/ticket/{ticket}/', 'TicketController@show');
 
 Route::get('/ticket/{ticket}/delete/', 'TicketController@destroy');
+
+Route::get('/ticket/{ticket}/edit/', 'TicketController@edit');
+
+Route::post('/ticket/{ticket}/edit/', 'TicketController@update');
