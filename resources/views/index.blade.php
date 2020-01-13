8391e518d6963ee3e945852efeb11767f4d35a78
Jacob Frampton-Skirrow <jacskir@gmail.com>
2020-01-13 15:25:08 +0000

    Add creation button to main listing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,6 +41,8 @@
 
     {{ $tickets -> links () }}
 
+    <a class="button" href="/add">Add a ticket</a>
+
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 </body>
 </html>
