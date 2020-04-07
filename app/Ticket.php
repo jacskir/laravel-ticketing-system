428de7759053832d0dfa99fed9a46d6eb499dc40
Jacob Skirrow <jacskir@gmail.com>
2020-04-07 22:40:40 +0100

    Tweak ticket database structure to utilise user_id

--- app/Ticket.php
+++ app/Ticket.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     protected $fillable = [
-        'name',
+        'user_id',
         'ticket',
         'status',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
