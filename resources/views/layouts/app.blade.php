7df0493ff0f2caa0c32b6dcf00ac1a0d68ba9472
Jacob Skirrow <jacskir@gmail.com>
2020-04-17 19:58:05 +0100

    Fix favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset ('images/favicon.png') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
     <link href="{{ asset ('css/ticketingsystem.css') }}" rel="stylesheet" />
     <title>@yield('page_title', 'Ticketing System')</title>
